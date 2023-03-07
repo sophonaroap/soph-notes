1e58984b20ebc685e61adf6fd6b8643a734af804
sophonaroap <sophie.machen@gmail.com>
2023-03-07 17:45:11 -0600

    * add nodemon & dev command * add cors * add endpoint

--- server/main/index.js
+++ server/main/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +9,14 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(cors());
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/message', (req, res) => {
+    res.json({message: 'Hello from server!'})
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
