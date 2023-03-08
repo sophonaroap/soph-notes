3ba5f6d9dbc8bff787f5c096cab2b7f537b72870
sophonaroap <sophie.machen@gmail.com>
2023-03-08 14:46:57 -0600

    * render /message response in App.tsx * notes * I like soph-notes as name

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,9 @@
 To run docker:
 https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
 
+To run using docker-compose:
+```shell
+docker-compose build --no-cache
+docker-compose up
+docker-compose down
+```
--- server/main/index.js
+++ server/main/index.js
@@ -1,23 +1,44 @@
 'use strict';
 
+// Import the express and cors modules
 const express = require('express');
 const cors = require('cors');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// Create an express app
+/*
+    This object, which is traditionally named app, has methods for routing HTTP requests, configuring middleware,
+    rendering HTML views, registering a template engine, and modifying application settings that control how the
+    application behaves (e.g. the environment mode, whether route definitions are case-sensitive, etc.)
+ */
 const app = express();
 app.use(cors());
+
+// Calling the express.json() method for parsing
+/*
+    express.json() is a built-in middleware function in Express.
+    This method is used to parse the incoming requests with JSON payloads and is based upon the bodyparser.
+ */
 app.use(express.json())
+
+/*
+    A route definition. The app.get() method specifies a callback function that will be invoked whenever there is an
+    HTTP GET request with a path ('/') relative to the site root. The callback function takes a request and a response
+    object as arguments, and calls send() on the response to return the string "Hello World!"
+ */
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
 app.get('/message', (req, res) => {
     res.json({message: 'Hello from server!'})
 })
 
+/*
+    Start the server on the specified port and logs the comment to the console.
+ */
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
