fae9081bc9d6d99af9310d1897142d617b20dfd9
sophonaroap <sophie.machen@gmail.com>
2023-03-14 13:00:11 -0600

    * create 1 author command

---
+++ server/main/init_dev_mongo.ts
@@ -0,0 +1,23 @@
+import {Prisma, PrismaClient} from "@prisma/client";
+
+export async function init_dev_mongo() {
+	console.log('Initializing dev mongo');
+
+	const prisma_client = new PrismaClient()
+
+	// Getting or creating initial user
+	const initialUser: Prisma.AuthorCreateInput = {
+		email: 'test_email@email.com',
+		name: 'Test User',
+	}
+
+	const returnedInitialUser = await prisma_client.author.upsert({
+		where: {
+			email: initialUser.email,
+		},
+		update: initialUser,
+		create: initialUser,
+	})
+
+	console.log('Updated or created initial user: ' + returnedInitialUser)
+}
