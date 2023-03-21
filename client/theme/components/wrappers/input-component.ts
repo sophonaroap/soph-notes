05562a44aa6b468d615ad87c29887191789e3112
sophonaroap <sophie.machen@gmail.com>
2023-03-20 18:06:54 -0600

    * create input-component.ts style overrides

---
+++ client/theme/components/wrappers/input-component.ts
@@ -0,0 +1,18 @@
+export const InputComponent = {
+	variants: {
+		login: {
+			addon: {
+				borderRadius: "0",
+			},
+			field: {
+				borderRadius: "0",
+			},
+			element: {
+				borderRadius: "0",
+			}
+		}
+	},
+	defaultProps: {
+		variant: "login"
+	}
+}
