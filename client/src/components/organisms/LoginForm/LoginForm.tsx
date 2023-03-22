b5f61a55118be296f2b6e8912f8e1b4bc0cf2e40
sophonaroap <sophie.machen@gmail.com>
2023-03-22 17:00:02 -0600

    * Beginning login form

--- client/src/components/organisms/LoginForm/LoginForm.tsx
+++ client/src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,5 +1,46 @@
+import {ChangeEvent, useState} from "react";
+import {FormControl} from "@chakra-ui/react";
+import EmailInput from "../../molecules/EmailInput";
+import PasswordInput from "../../molecules/PasswordInput";
+
 export default function LoginForm(props: any): JSX.Element {
-	return (
-		<></>
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setEmail(event.target.value)
+	}
+
+	const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value)
+	}
+
+	let emailInputProps = {
+		formControlProps: {
+			label: 'Email',
+			isRequired: true
+		},
+		inputProps: {
+			value: email,
+		},
+		onChange: handleEmailChange
+	}
+
+	let passwordInputProps = {
+		formControlProps: {
+			label: 'Password',
+			isRequired: true
+		},
+		inputProps: {
+			value: password,
+		},
+		onChange: handlePasswordChange
+	}
+
+		return (
+	<FormControl>
+		<EmailInput {...emailInputProps}/>
+		<PasswordInput {...passwordInputProps}/>
+	</FormControl>
 	)
 }
