88353d88ffece4a2ded28e93aa212166d9afee2d
sophonaroap <sophie.machen@gmail.com>
2023-03-22 16:45:46 -0600

    * index files for components simply export default

--- client/src/components/atoms/ButtonComponent/index.tsx
+++ client/src/components/atoms/ButtonComponent/index.tsx
@@ -1,11 +1,1 @@
-import {Button, useStyleConfig} from "@chakra-ui/react";
-
-export default function ButtonComponent(props: any): JSX.Element {
-	const { variant, ...rest } = props
-
-	let styles = useStyleConfig('ButtonComponent', { variant })
-
-	return (
-		<Button __css={styles} {...rest} />
-	)
-}
+export {default} from './ButtonComponent'
