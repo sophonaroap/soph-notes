a3e216ac410a6acc742d9e950838534c0e3961de
sophonaroap <sophie.machen@gmail.com>
2023-03-22 16:47:29 -0600

    * was having weird issues about deduped packages, but that was because I was importing React like pre-ecma 6 or whatever it is. This was me trying to fix it, but now things work so lets just use as is

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,12 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
