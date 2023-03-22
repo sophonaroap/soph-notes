6f685bbcb08ca860a6b1ef6954165f0e1eacc642
sophonaroap <sophie.machen@gmail.com>
2023-03-22 16:59:45 -0600

    * add to props correctly * formatting

--- client/src/components/molecules/PasswordInput/PasswordInput.tsx
+++ client/src/components/molecules/PasswordInput/PasswordInput.tsx
@@ -1,8 +1,8 @@
 import {useState} from "react";
 import {FormControl, FormLabel, IconButton, InputGroup, InputRightElement} from "@chakra-ui/react";
-import {ViewIcon, ViewOffIcon} from "@chakra-ui/icons";
 import InputComponent from "../../atoms/InputComponent";
 import {TPasswordInputProps} from "../types";
+import {ViewIcon, ViewOffIcon} from "@chakra-ui/icons";
 
 export default function PasswordInput(props: TPasswordInputProps): JSX.Element {
 	const [showPassword, setShowPassword] = useState<boolean>(props.showPassword || false)
@@ -15,27 +15,26 @@ export default function PasswordInput(props: TPasswordInputProps): JSX.Element {
 	let inputRightElement
 
 	if (props.showPassword) {
-		props.inputProps = {
-			...props.inputProps,
-			type: 'text'
-		}
+		props.inputProps!['type'] = 'text'
 		inputRightElement = (
-			<InputRightElement><IconButton aria-label="hide password" icon={<ViewOffIcon/>} onClick={onClickShowPassword} /></InputRightElement>
+			<InputRightElement>
+				<IconButton aria-label="hide password" icon={<ViewOffIcon/>} onClick={onClickShowPassword}/>
+			</InputRightElement>
 		)
 	} else {
-		props.inputProps = {
-			...props.inputProps,
-			type: 'password'
-		}
+		props.inputProps!['type'] = 'password'
 		inputRightElement = (
-			<InputRightElement><IconButton aria-label="show password" icon={<ViewIcon/>} onClick={onClickShowPassword} /></InputRightElement>
+			<InputRightElement>
+				<IconButton aria-label="show password" icon={<ViewIcon/>} onClick={onClickShowPassword}/>
+			</InputRightElement>
 		)
 	}
 	return (
 		<FormControl {...props.formControlProps}>
-			{props.formControlProps && props.formControlProps.label && <FormLabel>{props.formControlProps.label}</FormLabel>}
+			{props.formControlProps && props.formControlProps.label &&
+                <FormLabel>{props.formControlProps.label}</FormLabel>}
 			<InputGroup>
-				<InputComponent {...props.inputProps} onChange={props.onChangePassword} />
+				<InputComponent {...props.inputProps} onChange={props.onChange}/>
 				{inputRightElement}
 			</InputGroup>
 		</FormControl>
